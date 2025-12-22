8d8f2203a9cf49af09e734d8e9bbc8341320aec7
Vitaliy Elin <vitaliy.e@webkyte.com>
2025-12-22 14:34:28 +0200

    Add Russian(RU_ru) locale

--- update.sh
+++ update.sh
@@ -1,14 +1,19 @@
-#!/bin/sh
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-sh $SCRIPT_DIR/build.sh
-kpackagetool6 -u $SCRIPT_DIR/release/FancyTasks.tar.gz
+#!/bin/bash
+SCRIPT_DIR=$(dirname $(readlink -f "$0"))
 
-# Restart plasmashell
+echo "Compiling translations..."
+bash "$SCRIPT_DIR/package/translate/build"
+
+echo "Updating plasmoid ..."
+kpackagetool6 -t Plasma/Applet --upgrade "$SCRIPT_DIR/package"
+
+echo "Restarting Plasma..."
 if systemctl --user is-active --quiet plasma-plasmashell.service; then
-    systemctl --user restart plasma-plasmashell.service
+  systemctl --user restart plasma-plasmashell.service
 else
-    killall plasmashell
-    plasmashell > /dev/null 2>&1 &
+  plasmashell --replace > /dev/null 2>&1 &
+  disown
 fi
 
-sh ./iconinstall.sh
+bash "$SCRIPT_DIR/iconinstall.sh"
+echo "Update complete."
