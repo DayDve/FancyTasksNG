d38255cb0fc3f063314c57df6d8288967ca0fe84
Vitaliy Elin <daydve@smbit.pro>
2026-02-20 18:03:57 +0200

    fix: Update icon paths and rename old icon file
    - Move FansyTasksNG.svg inside package/ directory as FancyTasksNG.svg.
    - Delete unused legacy FancyTasks.png.
    - Update metadata.json to point to the new SVG.
    - Update iconinstall.sh to correctly place new SVG and update copyright.
    - Update README.md img tag paths respectively.

--- iconinstall.sh
+++ iconinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: 2023 Alexandra Stone <alexankitty@gmail.com>
+# SPDX-FileCopyrightText: 2025 Vitaliy Elin <daydve@smbit.pro>
 # SPDX-License-Identifier: GPL-2.0-or-later
 SCRIPT_DIR=$(dirname $(readlink -f "$0"))
-mkdir -p ~/.local/share/icons/hicolor/256x256/apps/
-cp "$SCRIPT_DIR/package/FancyTasks.png" ~/.local/share/icons/hicolor/256x256/apps/FancyTasks.png
+mkdir -p ~/.local/share/icons/hicolor/scalable/apps/
+cp "$SCRIPT_DIR/package/FancyTasksNG.svg" ~/.local/share/icons/hicolor/scalable/apps/FancyTasksNG.svg
 echo "Icon installed."
