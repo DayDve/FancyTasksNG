8460226694379060be26e96e94ab54064d57c883
Vitaliy Elin <daydve@smbit.pro>
2026-02-20 18:06:58 +0200

    chore: Improve Russian description and minor syntax fixes

--- iconinstall.sh
+++ iconinstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Alexandra Stone <alexankitty@gmail.com>
 # SPDX-FileCopyrightText: 2025 Vitaliy Elin <daydve@smbit.pro>
 # SPDX-License-Identifier: GPL-2.0-or-later
 SCRIPT_DIR=$(dirname $(readlink -f "$0"))
